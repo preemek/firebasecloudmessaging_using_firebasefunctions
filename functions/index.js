95975c7e64863e8a65365708c6dee680979254d4
Przemek <50340722+preemek@users.noreply.github.com>
2020-09-27 20:38:35 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,28 +2,12 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.sendNotificationToTopic = functions.firestore.document('puppies/{uid}').onWrite(async (event) => {
-    //let docID = event.after.id;
-    let title = event.after.get('title');
-    let content = event.after.get('content');
-    var message = {
-        notification: {
-            title: title,
-            body: content,
-        },
-        topic: 'namelesscoder',
-    };
-
-    let response = await admin.messaging().send(message);
-    console.log(response);
-});
-
-exports.sendNotificationToFCMToken = functions.firestore.document('messages/{mUid}').onWrite(async (event) => {
-    const uid = event.after.get('userUid');
-    const title = event.after.get('title');
-    const content = event.after.get('content');
+exports.sendNotificationToFCMToken = functions.firestore.document('users/{mUid}/followers').onWrite(async (event) => {
+    const uid = event.after.get('uid');
+    const title = 'title';
+    const content = 'content';
     let userDoc = await admin.firestore().doc(`users/${uid}`).get();
-    let fcmToken = userDoc.get('fcm');
+    let fcmToken = userDoc.get('token');
 
     var message = {
         notification: {
